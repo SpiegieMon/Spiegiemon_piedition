38d32be0cfa083d67a93739a35dd5770c7d6488b
EinDev <ich.bin@ein.dev>
2022-01-28 15:22:01 +0100

    Improve CPU performance

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ class Receiver(Thread):
 
     def run(self):
         while not self.quit_event.is_set():
-            select.select([self.node.ser], [self.node.ser], [self.node.ser], 0.5)
+            select.select([self.node.ser], [], [self.node.ser], 1)
             with self.lock:
                 self.node.receive()
 
