34854291d04968602743bced0c25716b8d211890
EinDev <ich.bin@ein.dev>
2022-01-28 16:31:30 +0100

    try to fix new line

--- reciever.py
+++ reciever.py
@@ -17,4 +17,5 @@ class Receiver(Thread):
             select.select([self.node.ser], [], [self.node.ser])
             with self.lock:
                 rssi, node, data = self.node.receive()
-                print(f"\nRecieved '{data.decode()}' from {node}\ninput: ")
+                print(f"\nRecieved '{data.decode()}' from {node}")
+                print('input: ', end='')
