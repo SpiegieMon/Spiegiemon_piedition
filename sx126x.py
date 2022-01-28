c1162620118ff071e185e42855e1d2a55ee0a646
EinDev <ich.bin@ein.dev>
2022-01-28 16:23:25 +0100

    fix set thread name

--- sx126x.py
+++ sx126x.py
@@ -257,7 +257,7 @@ class sx126x:
     """
     Returns a tuple with RSSI, Node ID and binary data
     """
-    def receive(self) -> (int, int, bytes):
+    def receive(self):
         if self.ser.inWaiting() > 0:
             time.sleep(0.5)
             r_buff = self.ser.read(self.ser.inWaiting())
