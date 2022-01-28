e01d13e395c82bbcb7a40b94fa4cc107b336359a
EinDev <ich.bin@ein.dev>
2022-01-28 13:21:51 +0100

    Reformat file

--- main.py
+++ main.py
@@ -4,15 +4,18 @@ import os
 import sys
 from threading import Thread, Lock
 
+
 def get_serial_tty():
     choices = ["/dev/ttyS0", "/dev/ttyAMA0"]
     for device in choices:
         if os.path.exists(device):
             return device
 
+
 node = sx126x.sx126x(serial_num=get_serial_tty(), freq=868, addr=100, power=22, rssi=True)
 
 node_lock = Lock()
+
 
 def send(data):
     with node_lock:
@@ -25,7 +28,8 @@ def input_loop():
         if text == 'q':
             break
         send(text)
-        
+
+
 input_thread = Thread(target=input_loop)
 input_thread.start()
 
